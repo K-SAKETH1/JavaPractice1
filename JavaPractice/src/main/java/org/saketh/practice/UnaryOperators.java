e2b01ac68a9493b50717ae8e997b83b595a5214e
saket <saket@Saketh>
2024-08-09 15:14:08 +0530

    Unary operators

---
+++ JavaPractice/src/main/java/org/saketh/practice/UnaryOperators.java
@@ -0,0 +1,17 @@
+package org.saketh.practice;
+
+public class UnaryOperators {
+	public static void main(String[] args)
+	{
+		//Pre increment
+		int a = 10;
+		int b = ++a;
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+		//Post increment
+		int c = 10;
+		int d = c++;
+		System.out.println("c: "+c);
+		System.out.println("d: "+d);
+	}
+}
