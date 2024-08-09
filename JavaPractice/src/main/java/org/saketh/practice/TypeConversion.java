78e4012852a92b5a5e224f872a4ae5d5f07b2063
K-SAKETH1 <110541580+K-SAKETH1@users.noreply.github.com>
2024-08-09 11:01:56 +0530

    Create TypeConversion.java

---
+++ JavaPractice/src/main/java/org/saketh/practice/TypeConversion.java
@@ -0,0 +1,14 @@
+package org.saketh.practice;
+import java.util.*;
+public class TypeConversion {
+	public static void main (String[] args)
+	{
+		//possible case
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		// we are storing integer in float and printing it
+		float printFloat = s.nextInt();
+		System.out.println("Float number: "+printFloat);
+		
+	}
+}
