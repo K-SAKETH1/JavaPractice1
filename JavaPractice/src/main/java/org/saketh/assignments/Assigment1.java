5711f1d6613caa29163b580a2e3f0ae0d2c0eedf
saket <saket@Saketh>
2024-08-09 12:38:40 +0530

    Assigment-1 and Assigment - 2 done

---
+++ JavaPractice/src/main/java/org/saketh/assignments/Assigment1.java
@@ -0,0 +1,18 @@
+package org.saketh.assignments;
+import java.util.*;
+public class Assigment1 {
+	/* In a program, input 3 numbers: A, B andC. You have to output the average of
+ these 3 numbers*/
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter A: ");
+		int a = s.nextInt();
+		System.out.print("Enter B: ");
+		int b = s.nextInt();
+		System.out.print("Enter C: ");
+		int c = s.nextInt();
+		int avg = (a+b+c)/3;
+		System.out.println("Average of A,B,C: "+avg);
+	}
+}
---
+++ JavaPractice/src/main/java/org/saketh/assignments/Assigment2.java
@@ -0,0 +1,14 @@
+package org.saketh.assignments;
+import java.util.*;
+public class Assigment2 {
+	/*In a program, input the side of a square. You have to output the area of the
+ square*/
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter side of a square: ");
+		int side = s.nextInt();
+		int area = side*side;
+		System.out.println("Area of the square: "+area);
+	}
+}
