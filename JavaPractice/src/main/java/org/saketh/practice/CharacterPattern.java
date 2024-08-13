172fd1f9a58548feaa98db6c81c8f3203de0646c
saket <saket@Saketh>
2024-08-13 19:39:40 +0530

    Character pattern

--- JavaPractice/src/main/java/org/saketh/practice/CharacterPattern.java
+++ JavaPractice/src/main/java/org/saketh/practice/CharacterPattern.java
@@ -6,7 +6,8 @@ public class CharacterPattern {
 		Scanner s = new Scanner(System.in);
 		System.out.print("Enter no of lines: ");
 		int lines = s.nextInt();
-		char ch = 'A';
+		System.out.print("Enter any character: ");
+		char ch = s.next().charAt(0);
 		for(int i=1;i<=lines;i++)
 		{
 			for(int j=1;j<=i;j++)
